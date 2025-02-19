fb4a703d058bb0c20691911ca57e3d3562184ac8
Mohit Rajan E <mhit98@gmail.com>
2025-02-19 20:31:23 +0530

    feat: add post tweet with headers fn

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,5 +1,5 @@
 use crate::api::error::TweetyError;
-use reqwest::{Client, Method};
+use reqwest::{header::HeaderMap, Client, Method};
 use reqwest_oauth1::{self, OAuthClientProvider};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -11,6 +11,11 @@ pub struct TweetyClient {
     pub(crate) access_token: String,
     pub(crate) consumer_key_secret: String,
     pub(crate) access_token_secret: String,
+}
+
+pub struct ResponseWithHeaders {
+    pub body: Value,
+    pub headers: HeaderMap,
 }
 
 impl TweetyClient {
@@ -105,6 +110,79 @@ impl TweetyClient {
                         .map_err(|err| TweetyError::JsonParseError(err.to_string()))?;
 
                     return Ok(api_response);
+                }
+                let status = response.status();
+
+                let status_text = response
+                    .json::<Value>()
+                    .await
+                    .map_err(|err| TweetyError::JsonParseError(err.to_string()))?;
+
+                Err(TweetyError::ApiError(format!(
+                    "HTTP {}: {}",
+                    status, status_text
+                )))
+            }
+            Err(err) => Err(TweetyError::NetworkError(err.to_string())),
+        }
+    }
+
+    pub(crate) async fn send_request_with_headers<T>(
+        &self,
+        url: &str,
+        method: Method,
+        body: Option<T>,
+    ) -> Result<ResponseWithHeaders, TweetyError>
+    where
+        T: Serialize + Deserialize<'static>,
+    {
+        if !self.is_initialized() {
+            return Err(TweetyError::MissingCredentials);
+        };
+
+        let parsed_url = match Url::parse(url) {
+            Ok(url) => url.to_string(),
+            Err(err) => {
+                return Err(TweetyError::UrlParseError(err));
+            }
+        };
+
+        let secrets = reqwest_oauth1::Secrets::new(&self.consumer_key, &self.consumer_key_secret)
+            .token(&self.access_token, &self.access_token_secret);
+
+        let client = Client::new();
+        let mut json_body = String::new();
+
+        if body.is_some() {
+            json_body = serde_json::to_string(&body).unwrap();
+        }
+
+        let response = match method {
+            Method::POST => client
+                .oauth1(secrets)
+                .post(&parsed_url)
+                .header("Content-Type", "application/json")
+                .body(json_body)
+                .send(),
+            Method::GET => client.oauth1(secrets).get(&parsed_url).send(),
+            Method::DELETE => client.oauth1(secrets).delete(&parsed_url).send(),
+            Method::PUT => client.oauth1(secrets).put(&parsed_url).send(),
+            _ => panic!("Method not allowed"),
+        };
+
+        match response.await {
+            Ok(response) => {
+                if response.status().is_success() {
+                    let headers = response.headers().clone();
+                    let api_response = response
+                        .json::<Value>()
+                        .await
+                        .map_err(|err| TweetyError::JsonParseError(err.to_string()))?;
+
+                    return Ok(ResponseWithHeaders {
+                        body: api_response,
+                        headers: headers,
+                    });
                 }
                 let status = response.status();
 
